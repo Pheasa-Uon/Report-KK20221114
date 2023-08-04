af2b032bbc9af0bcb12afddaefd22baff8f7bb50
pheasa <uonpheasa.up@gmail.com>
2023-08-04 11:25:51 +0700

    Add new report: Contract Will Renewal In 15 Days and Contract Late Renewal

---
+++ Report/Operation/ContractLateRenewal.aspx.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Data;
+using Report.Utils;
+using Report.Models;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace Report.Operation
+{
+    public partial class ContractLateRenewal : System.Web.UI.Page
+    {
+        private DBConnect db = new DBConnect();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataHelper.checkLoginSession();
+                DataHelper.populateBranchDDL(ddBranchName, DataHelper.getUserId());
+            }
+        }
+        private void GenerateReport(DataTable ContractLateRenewalDT)
+        {
+            ReportParameterCollection reportParameters = new ReportParameterCollection();
+            reportParameters.Add(new ReportParameter("Branch", ddBranchName.SelectedItem.Text));
+
+            var ContractLateRenewalDS = new ReportDataSource("ContractLateRenewalDS", ContractLateRenewalDT);
+
+            DataHelper.generateOperationReport(ReportViewer1, "ContractLateRenewal", reportParameters, ContractLateRenewalDS);
+
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+
+            var ContractLateRenewalSQL = "PS_ContractLateRenewal";
+
+            List<Procedure> parameters = new List<Procedure>();
+            parameters.Add(item: new Procedure() { field_name = "@pBranch", sql_db_type = MySqlDbType.VarChar, value_name = ddBranchName.SelectedItem.Value });
+
+            DataTable ContractLateRenewalDT = db.getProcedureDataTable(ContractLateRenewalSQL, parameters);
+            GenerateReport(ContractLateRenewalDT);
+        }
+    }
+}
---
+++ Report/Operation/ContractWillRenewalIn15Days.aspx.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Data;
+using Report.Utils;
+using Report.Models;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace Report.Operation
+{
+    public partial class ContractWillRenewalIn15Days : System.Web.UI.Page
+    {
+        private DBConnect db = new DBConnect();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataHelper.checkLoginSession();
+                DataHelper.populateBranchDDL(ddBranchName, DataHelper.getUserId());
+            }
+        }
+        private void GenerateReport(DataTable ContractWillRenewalIn15DaysDT)
+        {
+            ReportParameterCollection reportParameters = new ReportParameterCollection();
+            reportParameters.Add(new ReportParameter("Branch", ddBranchName.SelectedItem.Text));
+
+            var ContractWillRenewalIn15DaysDS = new ReportDataSource("ContractWillRenewalIn15DaysDS", ContractWillRenewalIn15DaysDT);
+
+            DataHelper.generateOperationReport(ReportViewer1, "ContractWillRenewalIn15Days", reportParameters, ContractWillRenewalIn15DaysDS);
+
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+
+            var ContractWillRenewalIn15DaysSQL = "PS_ContractWillRenewalIn15Days";
+
+            List<Procedure> parameters = new List<Procedure>();
+            parameters.Add(item: new Procedure() { field_name = "@pBranch", sql_db_type = MySqlDbType.VarChar, value_name = ddBranchName.SelectedItem.Value });
+
+            DataTable ContractWillRenewalIn15DaysDT = db.getProcedureDataTable(ContractWillRenewalIn15DaysSQL, parameters);
+            GenerateReport(ContractWillRenewalIn15DaysDT);
+        }
+    }
+}
